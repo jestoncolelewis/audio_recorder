6254a16a7bdafa01e49267313b1bcb53dd90196a
Jeston Lewis <jeston.lewis@protonmail.com>
2023-07-27 20:20:35 -0500

    fixed optionmenu

--- audio_recorder.py
+++ audio_recorder.py
@@ -3,7 +3,8 @@
 # monitor levels
 # monitor elapsed time
 
-import pyaudio
+import sounddevice as sd
+import numpy as np
 import wave
 from tkinter import (
     filedialog as fd,
@@ -13,16 +14,13 @@ from tkinter import (
     Tk,
     OptionMenu
 )
-from threading import Event
 
 # Global Variables
 chunk = 1024
-bitrate = pyaudio.paInt16
 channels = 1
 samplerate = 44100
 filename = "sample.wav"
 record = bool
-p = pyaudio.PyAudio()
 
 def start():
     record = True
@@ -30,29 +28,12 @@ def start():
     if record == True:
         print("Recording")
 
-        stream = p.open(
-            format=bitrate,
-            channels=channels,
-            rate=samplerate,
-            frames_per_buffer=chunk,
-            input=True
-        )
-
         frames = []
-
-        while record == True:
-            data = stream.read(chunk)
-            frames.append(data)
-
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
 
         print("Finished")
 
         wf = wave.open(filename, "wb")
         wf.setnchannels(channels)
-        wf.setsampwidth(p.get_sample_size(bitrate))
         wf.setframerate(samplerate)
         wf.writeframes(b''.join(frames))
         wf.close()
@@ -70,8 +51,9 @@ mainframe = ttk.Frame(window, padding="3 3 3 3")
 mainframe.grid(column=0, row=0, sticky='N S E W')
 
 var = StringVar()
-vals = ["obj1", "obj2", "obj3"]
-options = OptionMenu(mainframe, variable=var, value="test").grid(column=0, row=0)
+devices = sd.query_devices()
+var.set(devices[0]["name"])
+options = OptionMenu(mainframe, var, *devices).grid(column=0, row=0)
 ttk.Button(mainframe, text="START", command=start).grid(column=0, row=1)
 ttk.Button(mainframe, text="STOP", command=stop).grid(column=0, row=2)
 
